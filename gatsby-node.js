91c6659e2592d1c983c5cfe645829f866df6aa8e
Isabella Chen <miss.izzyc@gmail.com>
2018-08-31 12:35:13 +0200

    feat: programmatically creating blogposts

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,7 +1,44 @@
-/**
- * Implement Gatsby's Node APIs in this file.
- *
- * See: https://www.gatsbyjs.org/docs/node-apis/
- */
+const path = require('path')
+// const graphql = require('gatsby').graphql
 
- // You can delete this file if you're not using it
+const blogPostTemplate = path.resolve('src/templates/blog-post.js')
+
+const contentfulQuery = contentType => `
+  {
+    content: allContentful${contentType} (limit: 100) {
+      edges {
+        node {
+          slug
+        }
+      }
+    }
+  }
+`
+
+const pageSets = [
+  {
+    query: contentfulQuery('Blog'),
+    component: blogPostTemplate,
+    pathPrefix: 'blog/',
+  },
+]
+
+exports.createPages = async ({ graphql, actions }) => {
+  const { createPage } = actions
+  pageSets.forEach(async ({ query, component, pathPrefix }) => {
+    const response = await graphql(query)
+    if (response.errors) {
+      console.error(response.errors)
+      throw new Error(response.errors)
+    }
+    response.data.content.edges.forEach(({ node }) => {
+      createPage({
+        path: node.slug,
+        component,
+        context: {
+          slug: node.slug,
+        },
+      })
+    })
+  })
+}
