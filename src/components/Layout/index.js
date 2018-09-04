38d9a842dbcbb8f1e08e02094ff6a8d2b7b0ceae
Isabella Chen <miss.izzyc@gmail.com>
2018-09-04 19:58:42 +0200

    feat/update: layout - global styling in layout

--- src/components/Layout/index.js
+++ src/components/Layout/index.js
@@ -1,11 +1,26 @@
 import React, { Fragment } from 'react'
 import { StaticQuery, graphql } from 'gatsby'
 import Helmet from 'react-helmet'
-import styled from 'styled-components'
+import styled, { injectGlobal } from 'styled-components'
 
 import favicon from '../../assets/favicon.png'
 
 import NavBar from '../NavBar'
+
+injectGlobal`
+  body {
+    margin: 0;
+    padding: 0;
+    background-color: black;
+    color: white;
+  }
+
+  *,
+  *::before,
+  *::after {
+    box-sizing: border-box;
+  }
+`
 
 const Footer = styled.div`
   background-color: firebrick;
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react'
 import Link from 'gatsby-link'
 import { graphql } from 'gatsby'
-import styled, { injectGlobal } from 'styled-components'
+import styled from 'styled-components'
 
 import Head from '../components/Head'
 import EventList from '../components/EventsList'
@@ -11,106 +11,34 @@ import BlogsList from '../components/BlogsList'
 
 import Layout from '../components/Layout'
 
-injectGlobal`
-  body {
-    margin: 0;
-    padding: 0;
-  }
-
-  *,
-  *::before,
-  *::after {
-    box-sizing: border-box;
-  }
-`
-
 const MainContainer = styled.div`
   display: flex;
   flex-direction: column;
   background-color: black;
   color: white;
 `
-const NavContainer = styled.div`
-  position: relative;
-`
-const StyledNav = styled.div`
-  position: relative;
-  width: 100%;
-  height: 2rem;
-  background-color: firebrick;
-  padding: 0 1rem;
-  display: flex;
-  align-items: center;
-  justify-content: space-between;
-  z-index: 100;
-`
-const Menu = styled.div`
-  position: absolute;
-  top: 2rem;
-  background-color: blue;
-  left: 0;
-  width: 100%;
-  transition: transform 300ms cubic-bezier(0.68, -0.55, 0.265, 1.55);
-  transform: ${props => (props.isOpen ? 'none' : 'translateY(-100%)')};
-
-  & > ul {
-    margin: 0;
-    padding: 0;
-    list-style: none;
-  }
-`
 
 //get gatsby node to render features and pages
 //link to about us
 //dynamically link to features
 
-class Nav extends Component {
-  state = {
-    isOpen: false,
-  }
-
-  toggleMenu = () => {
-    this.setState(state => ({ isOpen: !state.isOpen }))
-  }
-
-  render() {
-    return (
-      <NavContainer>
-        <StyledNav>
-          <span>logo</span>
-          <button onClick={this.toggleMenu}>button</button>
-        </StyledNav>
-        <Menu isOpen={this.state.isOpen}>
-          <ul>
-            <li>about</li>
-            <li>contact</li>
-            <li>blog</li>
-          </ul>
-        </Menu>
-      </NavContainer>
-    )
-  }
-}
-
 const IndexPage = ({ data, location }) => {
   const { site, events, about, features, blogs } = data
   const eventsEdges = events.edges
   return (
-    <MainContainer>
+    <Layout>
       <Head
         site={site}
         pageTitle={'Home'}
         path={location.pathname}
         description={site.meta.description}
       />
-      <Layout>
-        <EventList events={eventsEdges} status={'live'} />
-        <FrontQuote about={about} />
-        <FeaturesList features={features} />
-        <BlogsList blogs={blogs} />
-        <Link to="/page-2/">Go to page 2</Link>
-      </Layout>
-    </MainContainer>
+      <EventList events={eventsEdges} status={'live'} />
+      <FrontQuote about={about} />
+      <FeaturesList features={features} />
+      <BlogsList blogs={blogs} />
+      <Link to="/page-2/">Go to page 2</Link>
+    </Layout>
   )
 }
 
