a9bf9c202e827c0fa06cb559310408ede54a45c1
Isabella Chen <miss.izzyc@gmail.com>
2018-09-23 16:22:12 +0200

    navbar/css: set z-index of dropdown to 100 so menu not hidden behind img

--- src/components/NavBar/index.js
+++ src/components/NavBar/index.js
@@ -12,7 +12,7 @@ const NavElements = styled.div`
   background-color: firebrick;
   width: 100%;
   height: 2rem;
-  z-index: 100;
+  z-index: 1000;
 `
 
 const Menu = styled.div`
@@ -20,6 +20,7 @@ const Menu = styled.div`
   background-color: blue;
   top: 2rem;
   width: 100%;
+  z-index: 100;
   transition: transform 300ms cubic-bezier(0.68, -0.55, 0.265, 1.55);
   transform: ${props => (props.isOpen ? 'none' : 'translateY(-100%)')};
   & > ul {
