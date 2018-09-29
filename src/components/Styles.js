f57abe169b49cbe90526a8fac038e189ce9ea9d8
Isabella Chen <miss.izzyc@gmail.com>
2018-09-29 17:37:18 +0200

    styling: added splash title and button to shared styles

--- src/components/Styles.js
+++ src/components/Styles.js
@@ -4,3 +4,24 @@ export const Title = styled.h1`
   font-family: 'Roboto', sans-serif;
   margin: 5px 0 10px 0;
 `
+
+export const SplashTitle = styled.h1`
+  font-family: 'Permanent Marker', sans-serif;
+  color: white;
+  font-size: 2.5rem;
+  margin: 5px 0 10px 0;
+  border-bottom: 1px solid white;
+`
+
+export const Button = styled.button`
+  background-color: gold;
+  border: none;
+  width: 100%;
+  font-size: 1.25rem;
+  padding: 10px 0 10px 0;
+  margin: 0 0 15px 0;
+  font-family: 'Roboto';
+  text-transform: uppercase;
+  font-weight: bold;
+  border-radius: 8px;
+`
