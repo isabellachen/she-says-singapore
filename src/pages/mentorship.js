c1fcde5cb07a364cdc4ebd2c7b6dd855c9025b15
Isabella Chen <miss.izzyc@gmail.com>
2018-10-21 17:55:41 +0200

    Chore: Style mentorship page

--- src/pages/mentorship.js
+++ src/pages/mentorship.js
@@ -5,6 +5,7 @@ import styled from 'styled-components'
 import Head from '../components/Head'
 import Layout from '../components/Layout'
 import { TeamMemberGrid } from '../components/TeamMember'
+import { Button } from '../components/Styles'
 
 const SignUpWrapper = styled.div`
   display: flex;
@@ -12,6 +13,31 @@ const SignUpWrapper = styled.div`
 
 const SignUp = styled.div`
   flex: 1;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  border: 5px solid gold;
+  margin: 0 8px 10px;
+  padding: 0 20px 20px;
+  & > h2 {
+    font-family: 'Permanent Marker', sans-serif;
+    font-size: 2em;
+    line-height: 1em;
+    text-align: center;
+    margin-bottom: 20px;
+  }
+`
+const SplashContainer = styled.div`
+  margin: 10px;
+`
+
+const MentoringInfo = styled.div`
+  margin: 10px;
+  & > h1 {
+    font-family: 'Permanent Marker';
+    margin: 0px;
+  }
 `
 
 const renderContactPerson = team => {
@@ -32,11 +58,22 @@ const Mentorship = ({ data, location }) => {
         path={location.pathname}
         description={site.meta.description}
       />
-      <Img sizes={heroImage.fluid} />
-      <p>{description.excerpt}</p>
+      <SplashContainer>
+        <Img sizes={heroImage.fluid} />
+      </SplashContainer>
+      <MentoringInfo>
+        <h1>Our Mission</h1>
+        {description.excerpt}
+      </MentoringInfo>
       <SignUpWrapper>
-        <SignUp>Be a Mentor</SignUp>
-        <SignUp>Be a Mentee</SignUp>
+        <SignUp>
+          <h2>Be a Mentor</h2>
+          <Button>Join Us</Button>
+        </SignUp>
+        <SignUp>
+          <h2>Be a Mentee</h2>
+          <Button>Sign Up</Button>
+        </SignUp>
       </SignUpWrapper>
       <TeamMemberGrid team={team} fromMentorship={true} />
       <h1>Questions?</h1>
